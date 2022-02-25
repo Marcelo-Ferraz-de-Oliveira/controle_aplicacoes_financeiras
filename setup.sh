bb3784278257ef0955aca7ee99e053998882c133
marcelo <mfogoiania@gmail.com>
2022-02-25 11:05:59 -0300

    Incluida verificaçao de instalaçao existente.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,23 @@
 #/bin/bash
+VENV_DIR="venv/"
+if [ -d "$VENV_DIR" ]; then
+    echo "Dependecies already installed."
+    echo "Starting servers..."
+    source venv/bin/activate
+else
+    echo "Installing dependecies and creating virtual python env in $VENV_DIR folder..."
+    sudo apt install default-jre python3.9 python3-pip chromium-chromedriver -y
+    python3 -m venv venv
+    source venv/bin/activate
+    cd flask-server
+    pip3 install -r requirements.txt
+    cp .flaskenv.example .flaskenv
+    cd ../react-frontend
+    sudo apt install nodejs npm -y
+    npm install
+cd ..
+fi
 cd flask-server
-sudo apt install default-jre python3.9 python3-pip chromium-chromedriver -y
-pip3 install -r requirements.txt
-cp .flaskenv.example .flaskenv
 flask run &
 cd ../react-frontend
-sudo apt install nodejs npm -y
-npm install
 npm start &
