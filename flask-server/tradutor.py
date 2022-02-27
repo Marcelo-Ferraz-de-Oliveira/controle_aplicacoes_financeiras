781fc9dff0dd36381eb3dabad5ae7e5e3075b44e
Marcelo Ferraz de Oliveira <mfogoiania@gmail.com>
2022-02-27 20:06:07 -0300

    Refatoração de nomes

--- flask-server/tradutor.py
+++ flask-server/tradutor.py
@@ -14,7 +14,7 @@ import pandas as pd
 #Função para construir string da expressão regular
 #A expressão buscará pela presença de qualquer um dos sufixos do Nome do Pregão
 # no formato: r'(ON|PN|CI).*$' (Quaquer um dos sufixos+quaisquer caracteres após o sufixo até o fim da string)
-def construir_string(codigos_b3_tipos):   
+def construir_re_string(codigos_b3_tipos):   
     string = r'('
     #Ordena pelo tamanho (len) da chave.
     #Assim, será analisado primeiro o sufixo maior (PNA antes de PN por exemplo)
@@ -26,27 +26,27 @@ def construir_string(codigos_b3_tipos):
     return string
 
 #Encontra o sufixo no Nome do Pregão, e retorna seu código
-def sufixo_para_numero(nome_pregao, r_string, codigos_b3_tipos):
+def sufixo_para_numero(nome_pregao, re_string, codigos_b3_tipos):
     try:
-        nome_pregao = re.findall(r_string,nome_pregao)[0]
+        nome_pregao = re.findall(re_string,nome_pregao)[0]
     except:
-        raise ValueError("Sufixo não encontrado")
+        raise ValueError(f"Sufixo não encontrado{nome_pregao}")
     
     return codigos_b3_tipos[nome_pregao]
 
 #Transforma o Nome do Pregão em Código de Negociação completo (prefixo+sufixo)
-def find_code(nome_pregao, r_string, codigos_b3, codigos_b3_tipos):
+def encontrar_codigo(nome_pregao, re_string, codigos_b3, codigos_b3_tipos):
     try:
         #Tenta encontrar o prefixo na tabela de códigos
-        novo_codigo = codigos_b3[re.sub(r_string,'',nome_pregao)] 
+        novo_codigo = codigos_b3[re.sub(re_string,'',nome_pregao)] 
     except:
         #Se o prefixo não está na tabela o próximo nome é o prefixo (Derivativos)
-        return re.sub(r_string,'',nome_pregao)
+        return re.sub(re_string,'',nome_pregao)
         
     return "".join(
             (
                 novo_codigo, 
-                sufixo_para_numero(nome_pregao, r_string, codigos_b3_tipos)
+                sufixo_para_numero(nome_pregao, re_string, codigos_b3_tipos)
                 )
             )
 #Função principal
@@ -57,9 +57,9 @@ def nome_pregao_to_codigo(string):
     with open('codigos_b3_tipos.json','r') as f:
         codigos_b3_tipos = json.load(f)['TckrSymb']
     #Cria a string de busca de sufixo
-    r_string = construir_string(codigos_b3_tipos)
+    RE_STRING = construir_re_string(codigos_b3_tipos)
     string = string.replace(" ", "")
-    return find_code(string, r_string, codigos_b3, codigos_b3_tipos)
+    return encontrar_codigo(string, RE_STRING, codigos_b3, codigos_b3_tipos)
 
 if __name__ == "__main__":
     string = ('ABEVB161ON 15,62', 'AMBEV S/AON', 'IRBRB330ON 3,30','ISHARE SP500CI','PETROBRASPN', 'USIMINASPNA')
