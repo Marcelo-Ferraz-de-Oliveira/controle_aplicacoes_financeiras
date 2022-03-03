30d58707e4892929dfcfddc471ad27f91cd3c087
Marcelo Ferraz de Oliveira <mfogoiania@gmail.com>
2022-03-02 21:33:16 -0300

    Adicionado script para parar os servidores de desenvolvimento

--- setup.sh
+++ setup.sh
@@ -3,19 +3,19 @@ VENV_DIR="venv/"
 if [ -d "$VENV_DIR" ]; then
     echo "Dependecies already installed."
     echo "Starting servers..."
-    source venv/bin/activate
+    source $VENV_DIR/bin/activate
 else
     echo "Installing dependecies and creating virtual python env in $VENV_DIR folder..."
     sudo apt install default-jre python3.9 python3-pip chromium-chromedriver -y
-    python3 -m venv venv
-    source venv/bin/activate
+    python3 -m venv $VENV_DIR
+    source $VENV_DIR/bin/activate
     cd flask-server
     pip3 install -r requirements.txt
     cp .flaskenv.example .flaskenv
     cd ../react-frontend
     sudo apt install nodejs npm -y
     npm install
-cd ..
+    cd ..
 fi
 cd flask-server
 flask run &
