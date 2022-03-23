52e375ae17e9f9966ec3a2b959d969067b0f2b22
Marcelo Ferraz de Oliveira <mfogoiania@gmail.com>
2022-03-23 12:20:20 -0300

    Display only active positions.

--- react_frontend/src/components/Posicoes.js
+++ react_frontend/src/components/Posicoes.js
@@ -14,42 +14,50 @@ const Posicoes = ({ posicoes, liquitadeOption }) => {
       <Container className="row g-4">
         <h4 className="text-center">Posição atual</h4>
         {Object.values(posicoes).map((posicao) => (
-          <Container className="col-md-6 col-lg-3" key={posicao.ativo}>
-            <ListGroup>
-              <ListGroup.Item>Ativo: {posicao.ativo}</ListGroup.Item>
-              <ListGroup.Item>Quantidade: {posicao.quantidade}</ListGroup.Item>
-              <ListGroup.Item>Vencimento: {posicao.prazo}</ListGroup.Item>
-              <ListGroup.Item>
-                Preço médio: R$
-                {toBR(posicao.preco_medio)}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                Valor: R$
-                {toBR(posicao.valor)}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                Expirado: {posicao.expirado === "true" ? "sim" : "não"}
-              </ListGroup.Item>
-              {posicao.expirado === "true" ? (
-                <ListGroup.Item>
-                  <Button
-                    variant="primary"
-                    onClick={() => liquitadeOption(posicao.ativo)}
-                  >
-                    Zerar Posição
-                  </Button>
-                </ListGroup.Item>
-              ) : (
-                <></>
-              )}
+          <>
+            {posicao.quantidade !== 0 ? (
+              <Container className="col-md-6 col-lg-3" key={posicao.ativo}>
+                <ListGroup>
+                  <ListGroup.Item>Ativo: {posicao.ativo}</ListGroup.Item>
+                  <ListGroup.Item>
+                    Quantidade: {posicao.quantidade}
+                  </ListGroup.Item>
+                  <ListGroup.Item>Vencimento: {posicao.prazo}</ListGroup.Item>
+                  <ListGroup.Item>
+                    Preço médio: R$
+                    {toBR(posicao.preco_medio)}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    Valor: R$
+                    {toBR(posicao.valor)}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    Expirado: {posicao.expirado === "true" ? "sim" : "não"}
+                  </ListGroup.Item>
+                  {posicao.expirado === "true" ? (
+                    <ListGroup.Item>
+                      <Button
+                        variant="primary"
+                        onClick={() => liquitadeOption(posicao.ativo)}
+                      >
+                        Zerar Posição
+                      </Button>
+                    </ListGroup.Item>
+                  ) : (
+                    <></>
+                  )}
 
-              {Object.entries(posicao.lucro).map(([data, lucro]) => (
-                <ListGroup.Item key={data}>
-                  Lucro no dia {data}: {toBR(lucro)}
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          </Container>
+                  {Object.entries(posicao.lucro).map(([data, lucro]) => (
+                    <ListGroup.Item key={data}>
+                      Lucro no dia {data}: {toBR(lucro)}
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </Container>
+            ) : (
+              <></>
+            )}
+          </>
         ))}
       </Container>
     </Section>
