2fbbfae9dc6b2e4c5d6e16c06cab430105a26866
marcelo <mfogoiania@gmail.com>
2022-05-17 22:00:46 -0300

    Fix headless test

--- python_backend/tests/test_headless_webdriver.py
+++ python_backend/tests/test_headless_webdriver.py
@@ -1,14 +1,15 @@
 import pytest
 from webapp.headless_webdriver import get_headless_selenium_webdriver
 URL_JSONTEST = "http://echo.jsontest.com/key/value/one/two"
-EXPECTED_RESULT = '<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">{\n   "one": "two",\n   "key": "value"\n}\n</pre></body></html>'
+EXPECTED_RESULT = '{\n   "one": "two",\n   "key": "value"\n}'
 WRONG_URL = "httq://wrongurlexample.inexistent"
 
 def test_get_headless_selenium_webdriver():
     driver = get_headless_selenium_webdriver()
     try:
         driver.get(URL_JSONTEST)
-        assert EXPECTED_RESULT == driver.page_source
+        result = driver.find_element_by_tag_name("pre").text
+        assert EXPECTED_RESULT == result
     finally:
         driver.close()
     with pytest.raises(Exception):
