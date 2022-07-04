f63edb0a6549cde155a520cd175dfa2fc121ae67
marcelo <mfogoiania@gmail.com>
2022-07-04 17:55:55 -0300

    Add pysondb to persist notes as json

--- python_backend/webapp/notas.py
+++ python_backend/webapp/notas.py
@@ -1,17 +1,19 @@
 from datetime import datetime
+from pysondb import db
 class Notas:
     def __init__(self) -> None:
-        self.__nota = []
+        self.__nota = db.getDb("nota.json")
     @property
     def nota(self) -> list:
-        
-        return_notas = sorted(self.__nota, key=lambda d: d["date_datetime"])
-        return [{key: nota[key] for key in nota if key != "date_datetime"} for nota in return_notas]
+        return_notas = sorted(list(self.__nota.getAll()), key=lambda d: datetime.strptime(d["date_datetime"], "%d/%m/%Y"))
+        return [{key: nota[key] for key in nota if key not in ("date_datetime", "id") } for nota in return_notas]
+
     
     def addNotas(self, new_notas: list) -> None:
         for new_nota in new_notas:
-            if new_nota not in self.__nota:
-                new_nota["date_datetime"] = datetime.strptime(new_nota["data"], "%d/%m/%Y")
-                self.__nota.append(new_nota)
+            if new_nota not in self.nota:
+                new_nota["date_datetime"] = datetime.strftime(datetime.strptime(new_nota["data"], "%d/%m/%Y"), "%d/%m/%Y")
+                self.__nota.add(new_nota)
+                # database.addMany(self.__nota)
             else:
                 raise ValueError(f"A nota nº {new_nota['nota']} da Corretora {new_nota['corretora']} já foi processada!")
