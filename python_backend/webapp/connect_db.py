a441076b7013a8f4d34fc837b544b5fe1b5a8e86
marcelo <mfogoiania@gmail.com>
2022-07-06 15:36:42 -0300

    Added support to MongoDB cloud database

---
+++ python_backend/webapp/connect_db.py
@@ -0,0 +1,26 @@
+from pymongo import mongo_client
+import os
+
+def get_database():
+
+    DB_USERNAME=str(os.getenv("DB_USERNAME"))
+    DB_PASSWORD=str(os.getenv("DB_PASSWORD"))
+    DB_CLUSTER_NAME=str(os.getenv("DB_CLUSTER_NAME"))
+    DB_BASE_NAME=str(os.getenv("DB_BASE_NAME"))
+
+    if "" in (DB_BASE_NAME, DB_CLUSTER_NAME, DB_PASSWORD, DB_USERNAME): raise ValueError ("Variáveis de ambiente com credenciais de banco não inseridas")
+
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@{DB_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority"
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    client = mongo_client.MongoClient(CONNECTION_STRING)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client[DB_BASE_NAME]
+
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":
+    # Get the database
+    dbname = get_database()
+    collection_name = dbname["notas"]
--- python_backend/webapp/notas.py
+++ python_backend/webapp/notas.py
@@ -1,19 +1,19 @@
 from datetime import datetime
-from pysondb import db
+from webapp.connect_db import get_database
 class Notas:
     def __init__(self) -> None:
-        self.__nota = db.getDb("nota.json")
+        self.__db = get_database()
+        self.__nota = self.__db['notas']
     @property
     def nota(self) -> list:
-        return_notas = sorted(list(self.__nota.getAll()), key=lambda d: datetime.strptime(d["date_datetime"], "%d/%m/%Y"))
-        return [{key: nota[key] for key in nota if key not in ("date_datetime", "id") } for nota in return_notas]
+        return_notas = sorted(list(self.__nota.find()), key=lambda d: datetime.strptime(d["date_datetime"], "%d/%m/%Y"))
+        return [{key: nota[key] for key in nota if key not in ("date_datetime", "_id") } for nota in return_notas]
 
     
     def addNotas(self, new_notas: list) -> None:
         for new_nota in new_notas:
             if new_nota not in self.nota:
                 new_nota["date_datetime"] = datetime.strftime(datetime.strptime(new_nota["data"], "%d/%m/%Y"), "%d/%m/%Y")
-                self.__nota.add(new_nota)
-                # database.addMany(self.__nota)
+                self.__nota.insert_one(new_nota)
             else:
                 raise ValueError(f"A nota nº {new_nota['nota']} da Corretora {new_nota['corretora']} já foi processada!")
