c25920ef35fb895e5128e717010d6b8aeb7f288b
Marcelo Ferraz <mfogoiania@gmail.com>
2023-03-04 17:08:07 -0300

    Remove B3 old tests

--- python_backend/tests/test_scraping_codigo_b3_tipos.py
+++ python_backend/tests/test_scraping_codigo_b3_tipos.py
@@ -1,6 +1,6 @@
-import pytest
-from webapp.scraping_codigo_b3_tipos import obter_CSV_B3
+#import pytest
+#from webapp.scraping_codigo_b3_tipos import obter_CSV_B3
 
-def test_obter_CSV_B3():
-    result = obter_CSV_B3()
-    assert result.empty ==  False
+#def test_obter_CSV_B3():
+#    result = obter_CSV_B3()
+#    assert result.empty ==  False
--- python_backend/tests/test_scraping_prices.py
+++ python_backend/tests/test_scraping_prices.py
@@ -1,10 +1,10 @@
-import pytest
-from webapp.scraping_prices import get_b3_prices
+#import pytest
+#from webapp.scraping_prices import get_b3_prices
 
-STOCK_CODES=["ABEV3"]
+#STOCK_CODES=["ABEV3"]
 
-def test_get_b3_prices():
-    prices = get_b3_prices(STOCK_CODES, query_interval=10)
-    result = list(next(prices).values())
-    assert result[0] != ""
+#def test_get_b3_prices():
+#    prices = get_b3_prices(STOCK_CODES, query_interval=10)
+#    result = list(next(prices).values())
+#    assert result[0] != ""
 
